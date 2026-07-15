413ce5cf958e0cecf10d94b6aa03f37f29ac8450
yusuf7700 <frefiregamer770@gmail.com>
2026-07-16 01:22:35 +0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Bu yerga Supabase loyihangizdan olingan ma'lumotlarni joylashtiring.
-// Supabase Dashboard > Project Settings > API bo'limidan topasiz.
-
-const SUPABASE_URL = "SIZNING_SUPABASE_URL_MANZILINGIZ"; // masalan: https://abcxyz.supabase.co
-const SUPABASE_ANON_KEY = "SIZNING_ANON_PUBLIC_KEY_INGIZ";
